2c9a1fd52468ba0274a035b706394654156d175b
Juliocrmks <57975456+Juliocrmks@users.noreply.github.com>
2021-10-13 23:14:13 -0500

    Add files via upload

---
+++ actividad 9/actividad 9/Document.swift
@@ -0,0 +1,21 @@
+//
+//  Document.swift
+//  actividad 9
+//
+//  Created by Blue People on 12/10/21.
+//
+
+import UIKit
+
+class Document: UIDocument {
+    
+    override func contents(forType typeName: String) throws -> Any {
+        // Encode your document with an instance of NSData or NSFileWrapper
+        return Data()
+    }
+    
+    override func load(fromContents contents: Any, ofType typeName: String?) throws {
+        // Load your document from contents
+    }
+}
+
---
+++ actividad 9/actividad 9/DocumentViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DocumentViewController.swift
+//  actividad 9
+//
+//  Created by Blue People on 12/10/21.
+//
+
+import UIKit
+
+class DocumentViewController: UIViewController {
+    
+    @IBOutlet weak var documentNameLabel: UILabel!
+    
+    var document: UIDocument?
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Access the document
+        document?.open(completionHandler: { (success) in
+            if success {
+                // Display the content of the document, e.g.:
+                self.documentNameLabel.text = self.document?.fileURL.lastPathComponent
+            } else {
+                // Make sure to handle the failed import appropriately, e.g., by presenting an error message to the user.
+            }
+        })
+    }
+    
+    @IBAction func dismissDocumentViewController() {
+        dismiss(animated: true) {
+            self.document?.close(completionHandler: nil)
+        }
+    }
+}
